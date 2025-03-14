d86d2994bef027fe4810ef7f71422f8952daeb5a
zakir981 <gonoiliavtrkopkakashka228@gmail.com>
2025-03-15 00:17:43 +0700

    vault backup: 2025-03-15 00:17:42

--- quartz.config.ts
+++ quartz.config.ts
@@ -24,8 +24,8 @@ const config: QuartzConfig = {
       fontOrigin: "googleFonts",
       cdnCaching: true,
       typography: {
-        header: "Montserrat",
-        body: "Montserrat",
+        header: "Oswald",
+        body: "Oswald",
         code: "IBM Plex Mono",
       },
       colors: {
