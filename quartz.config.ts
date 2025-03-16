c754bdd4e811a7dd04ad8f21ea1278f4aa15c489
zakir981 <gonoiliavtrkopkakashka228@gmail.com>
2025-03-16 11:57:28 +0700

    vault backup: 2025-03-16 11:57:28

--- quartz.config.ts
+++ quartz.config.ts
@@ -25,7 +25,7 @@ const config: QuartzConfig = {
       cdnCaching: true,
       typography: {
         header: "Oswald",
-        body: "Ubuntu",
+        body: "Nunito",
         code: "IBM Plex Mono",
       },
       colors: {
